adb49383dff0d17cb2b4c5181209c9d6910a0c5b
timstarkk <tim@timstark.dev>
2025-08-11 01:22:53 -0500

    Revert "coming soon banner"
    This reverts commit 91c30cc5043aa1a14bd2809a39aa689a56bff72c.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Navbar } from '@/components/Navbar'
-import ComingSoonBanner from '@/components/ComingSoonBanner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ComingSoonBanner />
+      <Navbar />{children}
       </body>
     </html>
   )
